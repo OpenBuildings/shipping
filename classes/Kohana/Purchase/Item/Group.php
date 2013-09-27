a291c1eb544c1cb02256d0502da15362f7493c71
Ivan Kerin <ivan@openbuildings.com>
2013-09-27 18:54:55 +0300

    add shipping_items_ids

--- classes/Kohana/Purchase/Item/Group.php
+++ classes/Kohana/Purchase/Item/Group.php
@@ -25,7 +25,10 @@ class Kohana_Purchase_Item_Group {
 	public function build_shipping_for(Model_Shipping_Method $method)
 	{
 		$shipping = $this->store_purchase->shipping;
+
 		$store_purchase_shipping = Jam::build('store_purchase_shipping', array('store_purchase' => $this->store_purchase));
+
+		// This is needed to counteract inverse_of store_purchase in store_purchase_shipping
 		$this->store_purchase->shipping = $shipping;
 
 		$store_purchase_shipping
@@ -34,9 +37,22 @@ class Kohana_Purchase_Item_Group {
 		return $store_purchase_shipping;
 	}
 
+	public function shipping_items_ids(Model_Shipping_Method $method)
+	{
+		$items = $this->shipping_items_for($method);
+		return array_map(function($items) {
+			return $item->id()
+		}, $items);
+	}
+
+	public function shipping_items_for(Model_Shipping_Method $method)
+	{
+		return $this->store_purchase->get_insist('shipping')->items_from($this->purchase_items);
+	}
+
 	public function is_method_selected(Model_Shipping_Method $method)
 	{
-		$items = $this->store_purchase->get_insist('shipping')->items_from($this->purchase_items);
+		$items = $this->shipping_items_for($method);
 
 		foreach ($items as $item) 
 		{
