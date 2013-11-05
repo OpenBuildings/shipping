fb69aeef816c8d41b4d757eff759914e7cd74499
Ivan Kerin <ivan@openbuildings.com>
2013-11-05 13:27:55 +0200

    Fix regression

--- classes/Kohana/Jam/Behavior/Shippable/Store/Purchase.php
+++ classes/Kohana/Jam/Behavior/Shippable/Store/Purchase.php
@@ -135,7 +135,7 @@ class Kohana_Jam_Behavior_Shippable_Store_Purchase extends Jam_Behavior {
 			}
 			$store_purchase->items = $store_purchase->items;
 
-			if ($store_purchase->shipping_address()->changed('country'))
+			if ($store_purchase->shipping_address()->changed('country') AND $store_purchase->shipping_address()->country)
 			{
 				$store_purchase->shipping->update_items_location($store_purchase->shipping_address()->country);
 			}
